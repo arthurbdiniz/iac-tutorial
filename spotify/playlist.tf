6704c68584815625d183ad8c59c2806762d82351
Arthur Diniz <arthurbdiniz@gmail.com>
2022-08-10 23:48:24 -0300

    Add spotify track search
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- spotify/playlist.tf
+++ spotify/playlist.tf
@@ -1,13 +1,24 @@
 resource "spotify_playlist" "playlist" {
-  name        = "My playlist"
-  description = "My playlist is so awesome"
+  name        = "IaC Playlist"
+  description = "My playlist created with IaC"
   public      = false
 
-  tracks = [
-    data.spotify_track.one_dance.id
-  ]
+  tracks = flatten([
+    data.spotify_track.one_dance.id,
+    data.spotify_search_track.the_killers.tracks[0].id
+  ])
 }
 
 data "spotify_track" "one_dance" {
   url = "https://open.spotify.com/track/1zi7xx7UVEFkmKfv06H8x0"
+}
+
+data "spotify_search_track" "the_killers" {
+  name   = "Somebody Told Me"
+  artist = "The Killers"
+  album  = "Hot Fuss"
+}
+
+output "show_track" {
+  value = data.spotify_search_track.the_killers.tracks
 }
